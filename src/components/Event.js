46a01d217b3ac561876baf1b84df6cc94dbac54b
kumackey <kumak1t09e0@gmail.com>
2020-04-12 17:34:19 +0900

    replace types names with constants

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import { DELETE_EVENT } from '../actions'
+
 const Event = ({ event, dispatch }) => {
   const id = event.id
 
   const handleClickDeleteButton = e => {
     const result = window.confirm(`このイベント(id=${id})を本当に削除しても良いですか？`)
-    if (result) { dispatch({ type: 'DELETE_EVENT', id }) }
+    if (result) { dispatch({ type: DELETE_EVENT, id }) }
   }
 
   return (
